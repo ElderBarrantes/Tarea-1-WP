c57d28f4035d6c0b82267f308a239fde4f7a4f38
XPC <XPC@DESKTOP-5399KO1>
2024-02-05 20:57:25 -0600

    Tarea sem 4

---
+++ src/main/java/com/zura/ZuraApplication.java
@@ -0,0 +1,13 @@
+package com.zura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuraApplication.class, args);
+	}
+
+}
